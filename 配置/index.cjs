773106eb7023022ce9ff4eecaff57a94cb99a2be
LiuYuao1 <119148973+LiuYuao1@users.noreply.github.com>
2022-11-27 13:24:14 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -8,56 +8,50 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxd20e218b23b6cd32',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'ed3dbc0933b04c73852068770d8c622a',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '禹州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '胡文梦',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJcPa6bSIsq1RwsOCYCoAMS8IOEc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'a7KdNt1rqQy351TVPBBNOUhbw-Yh3VdTm4mWce3KXCA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-17',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*2001-06-28', name: '胡文梦宝贝', year: '2001', date: '06-28',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*1999-06-05', name: '刘钰敖', year: '1999', date: '06-05',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '2022-06-18', name: '我们在一起已经', year: '2022', date: '06-18',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
+        { keyword: '我们在一起已经有', date: '2022-06-18' },
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'a7KdNt1rqQy351TVPBBNOUhbw-Yh3VdTm4mWce3KXCA',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJcPa6bSIsq1RwsOCYCoAMS8IOEc',
     }
   ],
 
